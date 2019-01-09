23d388ce499f30cadece8f75759106f2542c6f53
DrSmile444 <drsmile444@gmail.com>
2019-01-09 23:05:10 +0200

    added a listener to the searchOptions select

--- main.js
+++ main.js
@@ -15,27 +15,30 @@ const refs = {
    }
 };
 
-searchInput.addEventListener('input', () => {
-   const images = [
-       document.querySelector('.result-1'),
-       document.querySelector('.result-2')
-   ];
-   const option = searchOption.value;
-   let value = searchInput.value;
+searchInput.addEventListener('input', searchAndSet);
+searchOption.addEventListener('change', searchAndSet);
 
-   if (refs[option].replace) {
-      Object.entries(refs[option].replace).forEach(item => {
-         value = value.split(item[0]).join(item[1]);
-      })
-   }
+function searchAndSet() {
+    const images = [
+        document.querySelector('.result-1'),
+        document.querySelector('.result-2')
+    ];
+    const option = searchOption.value;
+    let value = searchInput.value;
 
-   if (refs[option].prefixes) {
-      refs[option].prefixes.forEach((prefix, i) => {
-         images[i].src = refs[option].link + value + prefix + '.' + refs[option].extension;
-      });
-   } else {
-      images[0].src = refs[option].link + value + '.' + refs[option].extension;
-      images[1].src = '';
-   }
-});
+    if (refs[option].replace) {
+        Object.entries(refs[option].replace).forEach(item => {
+            value = value.split(item[0]).join(item[1]);
+        })
+    }
+
+    if (refs[option].prefixes) {
+        refs[option].prefixes.forEach((prefix, i) => {
+            images[i].src = refs[option].link + value + prefix + '.' + refs[option].extension;
+        });
+    } else {
+        images[0].src = refs[option].link + value + '.' + refs[option].extension;
+        images[1].src = '';
+    }
+}
 
